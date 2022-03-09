35760f2f7a2880a71f15725f85a665788bef6a7d
Dmitrey Zadonskey <dmitrey.zadonskey@gmail.com>
2022-03-09 10:46:11 +0200

    icons conected

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+
 const PostListItem = ({label, important=false}) => {
 
 let classNames 
@@ -10,16 +15,21 @@ let classNames
             </span>
             <div className="d-flex justify-content-center align-items-center">
                 <button 
-                type="button"
-                className="btn-star btn-sm">
-                    <i class="fa fa-star"></i>   
+                type="button" className="btn-star btn-sm" >
+                    <FontAwesomeIcon icon={faStar} />
+                {/* // className="btn-star btn-sm"
+                //     <i class="fa fa-star"></i>    */}
                 </button>
+
                 <button 
-                type="button"
-                className="btn-trash btn-sm">
-                    <i className="fa fa-trash-o"></i>
+                type="button" className="btn-trash btn-sm">
+                    <FontAwesomeIcon icon={faTrashCan} />
+                {/* // className="btn-trash btn-sm">
+                //     <i className="fa fa-trash-o"></i> */}
                 </button>
-                <i className="fa fa-heart"></i>
+                
+                <i className="fa fa-heart"><FontAwesomeIcon icon={faHeart} /></i>
+                {/* <i className="fa fa-heart"></i> */}
                 </div>
         </li>
     )
