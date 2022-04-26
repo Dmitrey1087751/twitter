7fd37f3399362fd245a7dc8218169cd71f89615a
Dmitrey Zadonskey <dmitrey.zadonskey@gmail.com>
2022-04-26 10:41:18 +0200

    post-list-item to class in prougress

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -5,36 +5,42 @@ import { faStar } from "@fortawesome/free-regular-svg-icons";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
-const PostListItem = ({label, important=false}) => {
+export default class PostListItem extends React.Component {
 
-let classNames = 'app-list-item d-flex justify-content-between'
+    render() {
+        let classNames = 'app-list-item d-flex justify-content-between'
 
-if (important) {
-    classNames +=' important';
-}
+        if (important) {
+            classNames += ' important';
+        }
 
-    return (
-        <li className={classNames}>
-            <span className="app-list-item-lebel">
-                {label}
-            </span>
-            <div className="d-flex justify-content-center align-items-center">
-                <button 
-                type="button" className="btn-star btn-sm" >
-                    <FontAwesomeIcon icon={faStar} />
-               
-                </button>
+        const { label, important = false } = this.props
 
-                <button 
-                type="button" className="btn-trash btn-sm">
-                    <FontAwesomeIcon icon={faTrashCan} />
-              
-                </button>
-                
-                <i className="fa fa-heart"><FontAwesomeIcon icon={faHeart} /></i>
-               
+        return (
+            <li className={classNames}>
+                <span className="app-list-item-lebel">
+                    {label}
+                </span>
+
+                <div className="d-flex justify-content-center align-items-center">
+                    
+                    <button
+                        type="button" className="btn-star btn-sm" >
+                        <FontAwesomeIcon icon={faStar} />
+
+                    </button>
+
+                    <button
+                        type="button" className="btn-trash btn-sm">
+                        <FontAwesomeIcon icon={faTrashCan} />
+
+                    </button>
+
+                    <i className="fa fa-heart"><FontAwesomeIcon icon={faHeart} /></i>
+
                 </div>
-        </li>
-    )
+            </li>
+        )
+
+    }
 }
-export default PostListItem;
