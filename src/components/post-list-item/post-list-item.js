5915e57724a2a886dba38812cf026c5326bbe384
Dmitrey Zadonskey <dmitrey.zadonskey@gmail.com>
2022-04-26 08:39:59 +0200

    test commit from home PC

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -22,19 +22,17 @@ if (important) {
                 <button 
                 type="button" className="btn-star btn-sm" >
                     <FontAwesomeIcon icon={faStar} />
-                {/* // className="btn-star btn-sm"
-                //     <i class="fa fa-star"></i>    */}
+               
                 </button>
 
                 <button 
                 type="button" className="btn-trash btn-sm">
                     <FontAwesomeIcon icon={faTrashCan} />
-                {/* // className="btn-trash btn-sm">
-                //     <i className="fa fa-trash-o"></i> */}
+              
                 </button>
                 
                 <i className="fa fa-heart"><FontAwesomeIcon icon={faHeart} /></i>
-                {/* <i className="fa fa-heart"></i> */}
+               
                 </div>
         </li>
     )
